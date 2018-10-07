ee6ef90ae918ad843b27b2ab44baa62ddfe21bfe
sushrut009 <sushrut.21@tamu.edu>
2018-10-07 12:10:21 -0700

    Update UseParser.java

--- ParseArithmeticExpressions/UseParser.java
+++ ParseArithmeticExpressions/UseParser.java
@@ -22,27 +22,27 @@ public class UseParser {
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Stack<Integer> S = new Stack<Integer>();
-		ConvertInfixToPostfix C1 = new ConvertInfixToPostfix("2345-*+");
-		C1.PerformConversion();
-		String post = C1.getPostfix_string();
-		System.out.println(post);
-		int a ;
-		int b ;
-		int c;
-		for(int i=0;i<post.length();i++) {
-			if(C1.isOperator(C1.getPostfix_string().charAt(i))) {
-		      a = 	S.pop();
-		      b =   S.pop();
-		      c = operation(a,b,C1.getPostfix_string().charAt(i));
-		      S.push(c);
-			} else {
-				S.push((int)C1.getPostfix_string().charAt(i));
-			}
-		}
+	  // TODO Auto-generated method stub
+	  Stack<Integer> S = new Stack<Integer>();
+	  ConvertInfixToPostfix C1 = new ConvertInfixToPostfix("2345-*+");
+	  C1.PerformConversion();
+	  String post = C1.getPostfix_string();
+	  System.out.println(post);
+	  int a ;
+	  int b ;
+	  int c;
+	  for(int i=0;i<post.length();i++) {
+	    if(C1.isOperator(C1.getPostfix_string().charAt(i))) {
+	      a = S.pop();
+	      b = S.pop();
+	      c = operation(a,b,C1.getPostfix_string().charAt(i));
+	      S.push(c);
+	    } else {
+	      S.push(Character.getNumericValue(C1.getPostfix_string().charAt(i)));
+            }
+          }
 		
 		
-	}
+      }
 
 }
