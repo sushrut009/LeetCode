9bde5197015291f790edb209b0e7c3b62ab84e78
sushrut009 <sushrut.21@tamu.edu>
2018-10-22 22:21:58 -0700

    Update RemoveNthFromEnd.java

--- RemoveNthNodeFromEnd/RemoveNthFromEnd.java
+++ RemoveNthNodeFromEnd/RemoveNthFromEnd.java
@@ -1,19 +1,18 @@
 public class RemoveNthFromEnd {
-	//Approach : 
-	// Traverse the list and store the references to nodes in a hash map 
-	// Return the element at Length - N  index 
-	public void RemoveNthElemFromEnd(ListNode head , int n) {
-		ListNode current = head ;
-		HashMap<Integer,ListNode> hm = new HashMap<Integer,ListNode>(); 
-		int length = 0;  // Holds the length of the list
-		while(current != null) {
-			hm.put(length, current);
-			current = current.getNext();
-			length++ ;
-		}
-    
-    //Handle special cases ------------------------------------------------------------------------
-		if( n== 1) {
+  //Approach : 
+  // Traverse the list and store the references to nodes in a hash map 
+  // Return the element at Length - N  index 
+  public void RemoveNthElemFromEnd(ListNode head , int n) {
+    ListNode current = head ;
+    HashMap<Integer,ListNode> hm = new HashMap<Integer,ListNode>(); 
+    int length = 0;  // Holds the length of the list
+    while(current != null) {
+      hm.put(length, current);
+      current = current.getNext();
+      length++ ;
+    }
+  //Handle special cases ------------------------------------------------------------------------
+    if( n== 1) {
       if(length == 1) {
         head = null ; 
         return head ;
@@ -25,13 +24,12 @@ public class RemoveNthFromEnd {
       head =hm.get(1);
       return head;
     }
-		//  Special Cases End ---------------------------------------------------------------------------
-		// The hashMap has indices(keys) from 0 to length-1 
+  //  Special Cases End ---------------------------------------------------------------------------
+  // The hashMap has indices(keys) from 0 to length-1 
 		
-		//System.out.println(length );
-		int index = length - n ; 
-		hm.get(index-1).setNext(hm.get(index+1));
-		//System.out.printf("%dth element from the end of the list has value %d" ,n , hm.get(index).getData());
-	}
-
-}
+  //System.out.println(length );
+    int index = length - n ; 
+    hm.get(index-1).setNext(hm.get(index+1));
+  //System.out.printf("%dth element from the end of the list has value %d" ,n , hm.get(index).getData());
+  }
+}  
